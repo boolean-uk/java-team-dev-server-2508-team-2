7ca3fad30ada1e8e8a870a6d79c82c243658af93
Dave Ames <david.john.ames@gmail.com>
2025-07-25 11:12:29 +0100

    Basic logging in works

---
+++ src/test/java/com/booleanuk/TeamDevSim/TeamDevSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booleanuk.TeamDevSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamDevSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1846a78af01afd24f84ea713055716d4a84ab575
Jonathan van Oudheusden <jonathanvanoudheusden@gmail.com>
2025-09-05 16:24:01 +0200

    uncomment temp tests

--- src/test/java/com/booleanuk/TeamDevSim/TeamDevSimApplicationTests.java
+++ src/test/java/com/booleanuk/TeamDevSim/TeamDevSimApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TeamDevSimApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }

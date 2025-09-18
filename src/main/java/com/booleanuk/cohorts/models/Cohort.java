d28d21ccf91dec3d8991fca6de4999e8fdbd9157
Hanna Adenholm <hanna.adenholm@se.experis.com>
2025-09-18 09:58:36 +0200

    added name, startdate and enddate to cohorts

--- src/main/java/com/booleanuk/cohorts/models/Cohort.java
+++ src/main/java/com/booleanuk/cohorts/models/Cohort.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @NoArgsConstructor
 @Data
 @Entity
@@ -14,10 +16,25 @@ public class Cohort {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column
+    String name;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private java.util.Date startDate;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private java.util.Date endDate;
+
     @ManyToOne
     @JoinColumn(name = "specialisation_id", nullable = false)
     @JsonIncludeProperties({"id", "name"})
     private Specialisation specialisation;
+
+    @OneToMany(mappedBy = "cohort")
+    @JsonIncludeProperties({"id"})
+    private List<User> students;
 
     public Cohort(int id) {
         this.id = id;
